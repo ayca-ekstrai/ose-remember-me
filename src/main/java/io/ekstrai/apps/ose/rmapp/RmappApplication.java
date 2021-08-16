87e3be198bc21657490d450843caf332f16c2ec8
Ayca Kucukdurmaz <ayca@Aycas-MacBook-Pro.local>
2021-08-16 15:17:54 +0300

    initial commit

---
+++ src/main/java/io/ekstrai/apps/ose/rmapp/RmappApplication.java
@@ -0,0 +1,13 @@
+package io.ekstrai.apps.ose.rmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ekstrai/apps/ose/rmapp/RmappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ekstrai.apps.ose.rmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
